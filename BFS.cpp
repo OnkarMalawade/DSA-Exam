70926635553954ebb78981006b587ffdcdaa7aeb
OnkarMalawade <132775903+OnkarMalawade@users.noreply.github.com>
2024-01-28 15:34:21 +0530

    Update BFS.cpp

--- BFS.cpp
+++ BFS.cpp
@@ -1,58 +1,54 @@
-#include<iostream>
-#include<vector>
-#include<queue>
-
+#include <bits/stdc++.h>
 using namespace std;
-
-vector<int> bfs(const vector<vector<int> >& adj_list, int start, int target = -1) {
-    int n = adj_list.size();
-    vector<bool> visited(n, false);
-    vector<int> order;
-
-    queue<int> q;
-    q.push(start);
-    visited[start] = true;
-
-    while (!q.empty()) {
-        int u = q.front();
-        q.pop();
-        order.push_back(u);
-
-        for (int i = 0; i < adj_list[u].size(); ++i) {
-            int v = adj_list[u][i];
-            if (!visited[v]) {
-                q.push(v);
-                visited[v] = true;
-            }
-        }
-    }
-    if (target != -1 && !visited[target]) {
-        return {};
-    }
-    return order;
+class Graph {
+public:
+	int V;
+	vector<list<int> > adj;
+	Graph(int V);
+	void addEdge(int v, int w);
+	void BFS(int s);
+};
+Graph::Graph(int V)
+{
+	this->V = V;
+	adj.resize(V);
 }
-
-int main() {
-    vector<vector<int> > adj_list;
-
-    vector<int> row1 = [1, 2];
-    vector<int> row2 = [0, 3, 4};
-    vector<int> row3 = [0, 4};
-    vector<int> row4 = [1, 4};
-    vector<int> row5 = [1, 2, 3};
-
-    adj_list.push_back(row1);
-    adj_list.push_back(row2);
-    adj_list.push_back(row3);
-    adj_list.push_back(row4);
-    adj_list.push_back(row5);
-
-    vector<int> order = bfs(adj_list, 0);
-    cout << "\nBFS traversal: ";
-    for (int i = 0; i < order.size(); ++i) {
-        cout << order[i] << " ";
-    }
-    cout << endl;
-    return 0;
+void Graph::addEdge(int v, int w)
+{
+	adj[v].push_back(w);
 }
+void Graph::BFS(int s)
+{
+	vector<bool> visited;
+	visited.resize(V, false);
+	list<int> queue;
+	visited[s] = true;
+	queue.push_back(s);
 
+	while (!queue.empty()) {
+		s = queue.front();
+		cout << s << " ";
+		queue.pop_front();
+		for (auto adjacent : adj[s]) {
+			if (!visited[adjacent]) {
+				visited[adjacent] = true;
+				queue.push_back(adjacent);
+			}
+		}
+	}
+}
+int main()
+{
+	Graph g(4);
+	g.addEdge(0, 1);
+	g.addEdge(0, 2);
+	g.addEdge(1, 2);
+	g.addEdge(2, 0);
+	g.addEdge(2, 3);
+	g.addEdge(3, 3);
+
+	cout << "Following is Breadth First Traversal " << "(starting from vertex 2) \n";
+	g.BFS(2);
+
+	return 0;
+}
